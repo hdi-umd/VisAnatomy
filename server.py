91d02748c9c19e9a6f5ae7aea68a0c76f99a575e
Chen Chen <cchen24@umd.edu>
2023-11-06 19:34:10 +0800

    Update server.py

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ class MyHTTPRequestHandler(SimpleHTTPRequestHandler):
     def do_GET(self):
         print(self.path)
         if (self.path == "/"):#root path
-            self.path = "index.html" 
+            self.path = "annotationPage.html"
         elif self.path.startswith("/check_file_exists/"):
             filename = self.path.split('/')[-1]
             folder_path = "examples"
