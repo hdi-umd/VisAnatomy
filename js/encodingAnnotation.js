1cb220258055a8e5de1d623a9601ece1d7046d9f
Chen Chen <cchen24@umd.edu>
2024-01-19 13:01:19 -0500

    initialize the encoding annotation stage

--- js/encodingAnnotation.js
+++ js/encodingAnnotation.js
@@ -1,1 +1,9 @@
 function initilizeEncodingAnnotation() {}
+
+function toggleDot(dotElement) {
+  dotElement.classList.toggle("selected");
+}
+
+function recordSingleEncoding() {}
+
+function recordBatchEncoding() {}
