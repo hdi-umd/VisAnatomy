b22c63280953752b4b857829ab3e4b1f06b2c9a4
hannahbako <arri_hannah@yahoo.com>
2024-03-30 21:32:14 -0400

    made changes to automatically adjust size of svg within modal

--- website for the SVG corpus/static/js/svgformatter.js
+++ website for the SVG corpus/static/js/svgformatter.js
@@ -7,11 +7,18 @@ function displaySVG(text) {
   idMappings = {}; // Reset ID mappings
   indices = {}; // Reset indices
   const parent = document.getElementById("bkModalContentsvg")
-  const {width, height} = parent.getBoundingClientRect()
   parent.innerHTML=text
   let vis = d3.select("#bkModalContentsvg").select("svg")
+  const width = vis.attr("width").match(/(\d+)/)[0]
+  const height = vis.attr("height").match(/(\d+)/)[0]
+  var viewBox='0 0 1500 1000'
+  if (width && width>500){
+    // console.log(width, height)
+    viewBox='0 0 '+width+' '+height
+  }
+  // console.log("viewbox", viewBox)
   vis.attr("id", "vis")
-  .attr("viewBox", "0 0 700 680")
+  .attr("viewBox", viewBox)
   .attr("preserveAspectRatio", "xMidYMid meet")
   .attr("width", "100%")
   .style("position", "absolute")
