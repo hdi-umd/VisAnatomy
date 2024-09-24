d6727f4d037453bfe582e9f1b6aa19b2f863be92
Chen Chen <cchen24@umd.edu>
2024-09-24 15:51:12 -0400

    minor updates

--- _data_analysis_scripts/labelAnalysisPloting.py
+++ _data_analysis_scripts/labelAnalysisPloting.py
@@ -14,12 +14,15 @@ print('Number of charts:', len(allStat))
 
 # # plot the histogram of the number of main chart marks
 # # calculate the number of charts whose number of main chart marks is larger than 1000
-# num_mainChartMarks_larger_than_1000 = len([allStat[chartName]['num_mainChartMarks'] for chartName in allStat if allStat[chartName]['num_mainChartMarks'] > 1000])
+# num_mainChartMarks_larger_than_1000 = len([allStat[chartName]['num_mainChartMarks'] for chartName in allStat if allStat[chartName]['num_mainChartMarks'] > 500])
 # print('Number of charts whose number of main chart marks is larger than 1000:', num_mainChartMarks_larger_than_1000)
+# # calculate the maximum number of main chart marks
+# max_mainChartMarks = max([allStat[chartName]['num_mainChartMarks'] for chartName in allStat])
+# print('Maximum number of main chart marks:', max_mainChartMarks)
 # num_mainChartMarks = [allStat[chartName]['num_mainChartMarks'] for chartName in allStat]
 # # set size as 16:9
 # plt.figure(figsize=(16, 9))
-# plt.hist(num_mainChartMarks, bins=range(0, 1000, 10))
+# plt.hist(num_mainChartMarks, bins=range(0, 1500, 15))
 # # enlarge the font size of x and y axis labels and titles
 # plt.xticks(fontsize=24)
 # plt.yticks(fontsize=24)
@@ -33,9 +36,9 @@ print('Number of charts:', len(allStat))
 
 # # plot the histogram of the number of all elements
 # num_allElements = [allStat[chartName]['num_allElements'] for chartName in allStat]
-# # calculate the number of charts whose number of all elements is larger than 2000
-# num_allElements_larger_than_2000 = len([allStat[chartName]['num_allElements'] for chartName in allStat if allStat[chartName]['num_allElements'] > 2000])
-# print('Number of charts whose number of all elements is larger than 2000:', num_allElements_larger_than_2000)
+# # calculate the number of charts whose number of all elements is larger than 1000
+# num_allElements_larger_than_1000 = len([allStat[chartName]['num_allElements'] for chartName in allStat if allStat[chartName]['num_allElements'] > 1000])
+# print('Number of charts whose number of all elements is larger than 1000:', num_allElements_larger_than_1000)
 # plt.figure(figsize=(16, 9))
 # plt.hist(num_allElements, bins=range(0, 2000, 20))
 # plt.xticks(fontsize=24)
@@ -49,6 +52,9 @@ print('Number of charts:', len(allStat))
 
 # # plot the histogram of the ratio of main over all
 # ratio_main_over_all = [allStat[chartName]['ratio_main_over_all'] for chartName in allStat]
+# # get the number of ratios that is smaller than or equal to 0.4
+# num_ratios_smaller_than_04 = len([allStat[chartName]['ratio_main_over_all'] for chartName in allStat if allStat[chartName]['ratio_main_over_all'] >= 0.9])
+# print('Number of charts whose ratio of main over all is smaller than or equal to 0.4:', num_ratios_smaller_than_04)
 # plt.figure(figsize=(16, 9))
 # plt.hist(ratio_main_over_all, bins=np.arange(0, 1.1, 0.1))
 # plt.xticks(fontsize=24)
@@ -63,6 +69,9 @@ print('Number of charts:', len(allStat))
 # num_axes = [allStat[chartName]['num_axes'] for chartName in allStat]
 # # print the number of charts whose number of axes is larger than 20
 # num_axes_larger_than_20 = len([allStat[chartName]['num_axes'] for chartName in allStat if allStat[chartName]['num_axes'] > 20])
+# # get the number of charts whose number of axes is no larger than 2
+# num_axes_no_larger_than_2 = len([allStat[chartName]['num_axes'] for chartName in allStat if allStat[chartName]['num_axes'] <= 2])
+# print('Number of charts whose number of axes is no larger than 2:', num_axes_no_larger_than_2)
 # print('Number of charts whose number of axes is larger than 20:', num_axes_larger_than_20)
 # plt.figure(figsize=(16, 9))
 # plt.hist(num_axes, bins=range(0, 20, 1))
@@ -104,6 +113,7 @@ print('Number of charts:', len(allStat))
 #             unique_mainChartMarkTypes[markType] += 1
 # # sort the unique_mainChartMarkTypes by occurrence
 # unique_mainChartMarkTypes = dict(sorted(unique_mainChartMarkTypes.items(), key=lambda item: item[1], reverse=True))
+# print(unique_mainChartMarkTypes)
 # # plot the bar chart
 # plt.figure(figsize=(16, 9))
 # plt.bar(unique_mainChartMarkTypes.keys(), unique_mainChartMarkTypes.values())
@@ -120,79 +130,80 @@ print('Number of charts:', len(allStat))
 # plt.savefig('main_chart_mark_types_distribution.pdf')
 # plt.close()
 
-# # count unique_layoutTypes occurrence in all charts in the dataset and sort them by occurrence and plot the bar chart
-# unique_layoutTypes = {}
-# for chartName in allStat:
-#     for layoutType in allStat[chartName]['unique_layoutTypes']:
-#         if layoutType not in unique_layoutTypes:
-#             unique_layoutTypes[layoutType] = 1
-#         else:
-#             unique_layoutTypes[layoutType] += 1
-# # sort the unique_layoutTypes by occurrence
-# unique_layoutTypes = dict(sorted(unique_layoutTypes.items(), key=lambda item: item[1], reverse=True))
-# # remove both Encoded with Data and Layered keys from the dictionary
-# unique_layoutTypes.pop('Encoded with Data', None)
-# unique_layoutTypes.pop('Layered', None)
-# # plot the bar chart
-# plt.figure(figsize=(16, 9))
-# plt.bar(unique_layoutTypes.keys(), unique_layoutTypes.values())
-# plt.xticks(fontsize=24)
-# plt.yticks(fontsize=24)
-# plt.xlabel('Layout Types', fontsize=30)
-# # rotate the x axis labels for better readability
-# plt.xticks(rotation=45)
-# plt.ylabel('Number of Charts', fontsize=30)
-# # plt.title('Distribution of Layout Types')
-# plt.tight_layout()
-# plt.savefig('layouts_distribution.pdf')
-# plt.close()
-
-# for each mark type, pass all charts and record the occurrence of each encoding channel, and then plot the bar chart for each chart type
-unique_encodingChannels = {}
+# count unique_layoutTypes occurrence in all charts in the dataset and sort them by occurrence and plot the bar chart
+unique_layoutTypes = {}
 for chartName in allStat:
-    for markType in allStat[chartName]['unique_mainChartMarkTypes']:
-        if markType not in unique_encodingChannels:
-            unique_encodingChannels[markType] = {}
-        for encodingChannel in allStat[chartName]['unique_encodingChannels'][markType]:
-            if encodingChannel not in unique_encodingChannels[markType]:
-                unique_encodingChannels[markType][encodingChannel] = 1
-            else:
-                unique_encodingChannels[markType][encodingChannel] += 1
-
-# plot 10 sub figure for the 10 mark types, and place them into 2D
-num_subplots = len(unique_encodingChannels)
-num_rows = 3
-num_cols = 4
-
-fig, axs = plt.subplots(num_rows, num_cols, figsize=(32,32))
-# add a title for the whole figure
-# fig.suptitle('Distribution of Encoded Channels for Different Mark Types', fontsize=16)
-
-for i, markType in enumerate(unique_encodingChannels):
-    unique_encodingChannels[markType] = dict(sorted(unique_encodingChannels[markType].items(), key=lambda item: item[1], reverse=True))
-    row = i // num_cols
-    col = i % num_cols
-    axs[row, col].bar(unique_encodingChannels[markType].keys(), unique_encodingChannels[markType].values())
-    # Adjust x-axis label alignment and positioning
-    labels = axs[row, col].get_xticklabels()
-    axs[row, col].set_xticklabels(labels, rotation=-45, ha='left', rotation_mode='anchor', fontsize=24)
-    # set y-axis tick font size to be 24
-    axs[row, col].tick_params(axis='y', labelsize=24)
-    axs[row, col].set_ylabel('Number of Charts', fontsize=30)
-    axs[row, col].set_title(markType, fontsize=30)
-    
-    # Adjust the bottom margin to prevent text overlap
-    plt.subplots_adjust(bottom=0.15)
-    
-    # Increase vertical spacing between subplots
-    plt.subplots_adjust(hspace=0.8)
-
-# Remove any unused subplots
-for i in range(num_subplots, num_rows * num_cols):
-    row = i // num_cols
-    col = i % num_cols
-    fig.delaxes(axs[row, col])
-
+    for layoutType in allStat[chartName]['unique_layoutTypes']:
+        if layoutType not in unique_layoutTypes:
+            unique_layoutTypes[layoutType] = 1
+        else:
+            unique_layoutTypes[layoutType] += 1
+# sort the unique_layoutTypes by occurrence
+unique_layoutTypes = dict(sorted(unique_layoutTypes.items(), key=lambda item: item[1], reverse=True))
+# remove both Encoded with Data and Layered keys from the dictionary
+unique_layoutTypes.pop('Encoded with Data', None)
+unique_layoutTypes.pop('Layered', None)
+print(unique_layoutTypes)
+# plot the bar chart
+plt.figure(figsize=(16, 9))
+plt.bar(unique_layoutTypes.keys(), unique_layoutTypes.values())
+plt.xticks(fontsize=24)
+plt.yticks(fontsize=24)
+plt.xlabel('Layout Types', fontsize=30)
+# rotate the x axis labels for better readability
+plt.xticks(rotation=45)
+plt.ylabel('Number of Charts', fontsize=30)
+# plt.title('Distribution of Layout Types')
 plt.tight_layout()
-plt.savefig('encodingInfo.pdf')
+plt.savefig('layouts_distribution.pdf')
 plt.close()
+
+# # for each mark type, pass all charts and record the occurrence of each encoding channel, and then plot the bar chart for each chart type
+# unique_encodingChannels = {}
+# for chartName in allStat:
+#     for markType in allStat[chartName]['unique_mainChartMarkTypes']:
+#         if markType not in unique_encodingChannels:
+#             unique_encodingChannels[markType] = {}
+#         for encodingChannel in allStat[chartName]['unique_encodingChannels'][markType]:
+#             if encodingChannel not in unique_encodingChannels[markType]:
+#                 unique_encodingChannels[markType][encodingChannel] = 1
+#             else:
+#                 unique_encodingChannels[markType][encodingChannel] += 1
+
+# # plot 10 sub figure for the 10 mark types, and place them into 2D
+# num_subplots = len(unique_encodingChannels)
+# num_rows = 3
+# num_cols = 4
+
+# fig, axs = plt.subplots(num_rows, num_cols, figsize=(32,32))
+# # add a title for the whole figure
+# # fig.suptitle('Distribution of Encoded Channels for Different Mark Types', fontsize=16)
+
+# for i, markType in enumerate(unique_encodingChannels):
+#     unique_encodingChannels[markType] = dict(sorted(unique_encodingChannels[markType].items(), key=lambda item: item[1], reverse=True))
+#     row = i // num_cols
+#     col = i % num_cols
+#     axs[row, col].bar(unique_encodingChannels[markType].keys(), unique_encodingChannels[markType].values())
+#     # Adjust x-axis label alignment and positioning
+#     labels = axs[row, col].get_xticklabels()
+#     axs[row, col].set_xticklabels(labels, rotation=-45, ha='left', rotation_mode='anchor', fontsize=24)
+#     # set y-axis tick font size to be 24
+#     axs[row, col].tick_params(axis='y', labelsize=24)
+#     axs[row, col].set_ylabel('Number of Charts', fontsize=30)
+#     axs[row, col].set_title(markType, fontsize=30)
+    
+#     # Adjust the bottom margin to prevent text overlap
+#     plt.subplots_adjust(bottom=0.15)
+    
+#     # Increase vertical spacing between subplots
+#     plt.subplots_adjust(hspace=0.8)
+
+# # Remove any unused subplots
+# for i in range(num_subplots, num_rows * num_cols):
+#     row = i // num_cols
+#     col = i % num_cols
+#     fig.delaxes(axs[row, col])
+
+# plt.tight_layout()
+# plt.savefig('encodingInfo.pdf')
+# plt.close()
